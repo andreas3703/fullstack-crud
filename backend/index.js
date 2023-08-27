3c7174891ab36001f24f01ebca8a61865a7df9f1
andreas3703 <143333713+andreas3703@users.noreply.github.com>
2023-08-27 20:47:30 +0700

    initial project

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send("Hello node js");
+});
+
+app.listen(3000, () => {
+  console.log("Backend is running");
+});
